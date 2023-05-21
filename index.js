bcc7ced4b0d258998e1d5585342674705c1c813c
Anirudh Sharma <126901961+anisharma07@users.noreply.github.com>
2023-05-21 23:44:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+var random=Math.floor(6*Math.random())+1;
+document.querySelector(".img1").setAttribute("src", "images/dice"+random+".png");
+
+var random2=Math.floor(6*Math.random())+1;
+document.querySelector(".img2").setAttribute("src", "images/dice"+random2+".png");
+
+if(random>random2){
+    document.querySelector("h1").innerHTML="Player1 Wins!";
+}
+else if(random<random2){
+    document.querySelector("h1").innerHTML="Player2 Wins!";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!";
+}
